98fa71cc5a2639472e1ff9fcb06a64b3d1adb8a6
dmriveroe <dmriveroe@unal.edu.co>
2024-09-17 00:56:25 -0500

    Final

---
+++ activate_env.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Activate conda environment
+CALL conda activate IAMINI
+
+REM Optionally, you can navigate to your project directory
+cd /d path\to\your\project
+
+REM Keep the command prompt open
+cmd /k
